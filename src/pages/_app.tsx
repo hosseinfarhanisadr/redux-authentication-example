85ebafb0c2cd6eaec2a459369b6d4d399898de61
Hossein <hosseinfarhanisadr@gmail.com>
2022-01-18 17:27:54 +0330

    Add material-ui with custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,29 @@
-import Head from "next/head";
-import { AppProps } from "next/app";
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../theme';
+import { createEmotionCache } from '../utils/create-emotion-cache';
 
-export default function MyApp(props: AppProps) {
-  const { Component, pageProps } = props;
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <Component {...pageProps} />
-    </>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
